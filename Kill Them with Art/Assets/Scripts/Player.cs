5796c9f8a58e01db336e513e6fa3a1686028c1b0
Yu Jo <zoey5538@gmail.com>
2017-08-03 20:19:49 +0200

    Add Spawn Point, Player script with toggles

---
+++ Kill Them with Art/Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class ToggleEnabled : UnityEvent<bool> { }
+
+public class Player : NetworkBehaviour {
+
+    //use dynamic bool in editor
+    [SerializeField] ToggleEnabled onToggleShared; //everyone
+    [SerializeField] ToggleEnabled onToggleLocal; //the local player
+    [SerializeField] ToggleEnabled onToggleRemote; //the remote players
+
+    void Start()
+    {
+        EnablePlayer();
+    }
+
+    void EnablePlayer()
+    {
+        onToggleShared.Invoke(true);
+
+        if(isLocalPlayer)
+        {
+            onToggleLocal.Invoke(true);
+        } else
+        {
+            onToggleRemote.Invoke(true);
+        }
+    }
+
+    void DisablePlayer()
+    {
+        onToggleShared.Invoke(false);
+
+        if (isLocalPlayer)
+        {
+            onToggleLocal.Invoke(false);
+        }
+        else
+        {
+            onToggleRemote.Invoke(false);
+        }
+    }
+}
